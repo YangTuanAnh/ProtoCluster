1ea04d3357b234584a58fe864203341d5607b21c
YangTuanAnh <78615512+YangTuanAnh@users.noreply.github.com>
2025-08-26 18:14:08 +0700

    Update download_data.sh

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -9,12 +9,12 @@ curl -O https://shrec2025.drugdesign.fr/files/train_set.csv
 curl -O https://shrec2025.drugdesign.fr/files/test_set.csv
 
 # Download the VTK tar.gz archives
-curl -O https://shrec2025.drugdesign.fr/files/train_set_vtk.tar.gz
-curl -O https://shrec2025.drugdesign.fr/files/test_set_vtk.tar.gz
+curl -O https://shrec2025.drugdesign.fr/files/train_set.tar.xz
+curl -O https://shrec2025.drugdesign.fr/files/test_set.tar.xz
 
 # Extract the archives (into separate folders)
 mkdir -p train_set_vtk
 mkdir -p test_set_vtk
 
-tar -xzf train_set_vtk.tar.gz -C train_set_vtk
-tar -xzf test_set_vtk.tar.gz -C test_set_vtk
+tar -xzf train_set.tar.xz -C train_set_vtk
+tar -xzf test_set.tar.xz -C test_set_vtk
